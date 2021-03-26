dc21f5e556e2ffb0e89151d1ac1e18a9adfab30c
Christopher Poenaru <kiambogo@gmail.com>
2021-03-26 13:26:32 -0700

    return pointer to buffer for page getter (#1)

--- mempager/pager_test.go
+++ mempager/pager_test.go
@@ -26,7 +26,7 @@ func Test_Pager_DefaultPageSize(t *testing.T) {
 	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be true")
 	assert.Equal(t, (*Page)(nil), pgr.Get(12), "Get() should return nil")
 	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be still be true")
-	assert.Equal(t, &Page{offset: DEFAULT_PAGE_SIZE * 12}, pgr.GetOrAlloc(12), "GetOrAlloc() should return newly allocated page")
+	assert.Equal(t, &Page{offset: DEFAULT_PAGE_SIZE * 12, buffer: make([]byte, DEFAULT_PAGE_SIZE)}, pgr.GetOrAlloc(12), "GetOrAlloc() should return newly allocated page")
 	assert.Equal(t, false, pgr.IsEmpty(), "Empty() should be now be false")
 	assert.Equal(t, 13, pgr.Len(), "Len() should be 13")
 
@@ -46,11 +46,11 @@ func Test_Pager_CustomPageSize(t *testing.T) {
 	pageSize := 512
 	pgr := NewPager(pageSize)
 
-	assert.Equal(t, pageSize, pgr.PageSize(), "PageSize() should be %s", DEFAULT_PAGE_SIZE)
+	assert.Equal(t, pageSize, pgr.PageSize(), "PageSize() should be %s", pageSize)
 	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be true")
 	assert.Equal(t, (*Page)(nil), pgr.Get(12), "Get() should return nil")
 	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be still be true")
-	assert.Equal(t, &Page{offset: pageSize * 12}, pgr.GetOrAlloc(12), "GetOrAlloc() should return newly allocated page")
+	assert.Equal(t, &Page{offset: pageSize * 12, buffer: make([]byte, 512)}, pgr.GetOrAlloc(12), "GetOrAlloc() should return newly allocated page")
 	assert.Equal(t, false, pgr.IsEmpty(), "Empty() should be now be false")
 	assert.Equal(t, 13, pgr.Len(), "Len() should be 13")
 
@@ -62,4 +62,37 @@ func Test_Pager_CustomPageSize(t *testing.T) {
 
 	pgr.Set(3, make([]byte, 1000))
 	assert.Equal(t, &Page{offset: pageSize * 3, buffer: make([]byte, pageSize)}, pgr.Get(3), "Set() should truncate the provided buffer down to the page size")
+}
+
+func Test_Pager_SetBytesOnPage(t *testing.T) {
+	t.Parallel()
+
+	pgr := NewPager(0)
+
+	page := pgr.GetOrAlloc(1)
+	buf := page.Buffer()
+
+	expected := make([]byte, DEFAULT_PAGE_SIZE)
+	for i, byte := range []byte("hello!") {
+		(*buf)[i] = byte
+		expected[i] = byte
+	}
+
+	checkpage := pgr.Get(1)
+	buf = checkpage.Buffer()
+	assert.Equal(t, expected, *buf)
+}
+
+func Test_Pager_CantReplacePageBufferPointer(t *testing.T) {
+	t.Parallel()
+
+	pgr := NewPager(0)
+
+	page := pgr.GetOrAlloc(1)
+	buf := page.Buffer()
+	*buf = []byte("foobar")
+
+	checkpage := pgr.Get(1)
+	buf = checkpage.Buffer()
+	assert.Equal(t, make([]byte, DEFAULT_PAGE_SIZE), *buf)
 }
