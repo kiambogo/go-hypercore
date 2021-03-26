9a32ba682b07cf8a0b9b854f4ece422ac7eddb6c
Christopher Poenaru <kiambogo@gmail.com>
2021-03-25 18:39:10 -0700

    add mempgr

---
+++ pager.go
@@ -0,0 +1,102 @@
+package hypercore
+
+const DEFAULT_PAGE_SIZE = 1024
+
+// Page is an indexed representation of a chunk of memory
+type Page struct {
+	offset int
+	buffer []byte
+}
+
+// Offset returns the byte offset of the page relvative to the other pages within the pager
+func (p Page) Offset() int {
+	return p.offset
+}
+
+// Buffer returns the internal buffer for the page
+func (p Page) Buffer() []byte {
+	return p.buffer
+}
+
+// Pager is a tool used to reference chunks of memory (pages)
+// Allows retrieval, allocation, and setting memory contents by an index
+type Pager struct {
+	pageSize int
+	pages    []*Page
+}
+
+// NewPager constructs a new pager with the specified pageSize
+// Defaults the page size to 1024 bytes if passed a size of 0
+func NewPager(pageSize int) Pager {
+	if pageSize == 0 {
+		pageSize = DEFAULT_PAGE_SIZE
+	}
+	return Pager{
+		pageSize: pageSize,
+		pages:    []*Page{},
+	}
+}
+
+func (p Pager) newPage(index int, buf []byte) *Page {
+	return &Page{
+		offset: index * p.pageSize,
+		buffer: buf,
+	}
+}
+
+// Get will return the page at the specified index
+func (p Pager) Get(pageNum int) *Page {
+	if pageNum >= len(p.pages) {
+		return nil
+	}
+
+	return p.pages[pageNum]
+}
+
+// GetOrAlloc will return the page at the specified index, allocating it if not already allocated
+func (p *Pager) GetOrAlloc(pageNum int) (page *Page) {
+	p.growPages(pageNum)
+
+	if page = p.pages[pageNum]; page == nil {
+		p.pages[pageNum] = p.newPage(pageNum, nil)
+		page = p.pages[pageNum]
+	}
+
+	return page
+}
+
+// PageSize will return the page size of the pager
+func (p Pager) PageSize() int {
+	return p.pageSize
+}
+
+// Len will return the size of the pager (number of pages)
+func (p Pager) Len() int {
+	return len(p.pages)
+}
+
+// IsEmpty will check if the memory page is empty (has zero pages)
+func (p Pager) IsEmpty() bool {
+	return len(p.pages) == 0
+}
+
+// Set will set the contents of the page at the specified index, with the provided data
+// Allocates a new page if it doesn't already exist
+func (p *Pager) Set(pageNum int, data []byte) {
+	page := p.GetOrAlloc(pageNum)
+	page.buffer = p.truncate(data)
+}
+
+// growPages will increases the size of the pager's page buffer up till the supplied index
+func (p *Pager) growPages(index int) {
+	for i := len(p.pages); i <= index; i++ {
+		p.pages = append(p.pages, nil)
+	}
+}
+
+func (p Pager) truncate(buf []byte) []byte {
+	if p.pageSize >= len(buf) {
+		return buf
+	}
+	return buf[:p.pageSize]
+}
---
+++ pager_test.go
@@ -0,0 +1,65 @@
+package hypercore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Page(t *testing.T) {
+	t.Parallel()
+
+	p := Page{
+		offset: 123,
+		buffer: []byte{},
+	}
+
+	assert.Equal(t, 123, p.Offset(), "Page offset incorrect")
+}
+
+func Test_Pager_DefaultPageSize(t *testing.T) {
+	t.Parallel()
+
+	pgr := NewPager(DEFAULT_PAGE_SIZE)
+
+	assert.Equal(t, DEFAULT_PAGE_SIZE, pgr.PageSize(), "PageSize() should be %s", DEFAULT_PAGE_SIZE)
+	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be true")
+	assert.Equal(t, (*Page)(nil), pgr.Get(12), "Get() should return nil")
+	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be still be true")
+	assert.Equal(t, &Page{offset: DEFAULT_PAGE_SIZE * 12}, pgr.GetOrAlloc(12), "GetOrAlloc() should return newly allocated page")
+	assert.Equal(t, false, pgr.IsEmpty(), "Empty() should be now be false")
+	assert.Equal(t, 13, pgr.Len(), "Len() should be 13")
+
+	pgr.Set(2, []byte("hello world"))
+	assert.Equal(t, &Page{offset: DEFAULT_PAGE_SIZE * 2, buffer: []byte("hello world")}, pgr.Get(2), "Get() should return the set page")
+
+	pgr.Set(20, []byte("foo bar"))
+	assert.Equal(t, &Page{offset: DEFAULT_PAGE_SIZE * 20, buffer: []byte("foo bar")}, pgr.Get(20), "Get() should return the set page")
+
+	pgr.Set(3, make([]byte, DEFAULT_PAGE_SIZE+10))
+	assert.Equal(t, &Page{offset: DEFAULT_PAGE_SIZE * 3, buffer: make([]byte, DEFAULT_PAGE_SIZE)}, pgr.Get(3), "Set() should truncate the provided buffer down to the page size")
+}
+
+func Test_Pager_CustomPageSize(t *testing.T) {
+	t.Parallel()
+
+	pageSize := 512
+	pgr := NewPager(pageSize)
+
+	assert.Equal(t, pageSize, pgr.PageSize(), "PageSize() should be %s", DEFAULT_PAGE_SIZE)
+	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be true")
+	assert.Equal(t, (*Page)(nil), pgr.Get(12), "Get() should return nil")
+	assert.Equal(t, true, pgr.IsEmpty(), "Empty() should be still be true")
+	assert.Equal(t, &Page{offset: pageSize * 12}, pgr.GetOrAlloc(12), "GetOrAlloc() should return newly allocated page")
+	assert.Equal(t, false, pgr.IsEmpty(), "Empty() should be now be false")
+	assert.Equal(t, 13, pgr.Len(), "Len() should be 13")
+
+	pgr.Set(2, []byte("hello world"))
+	assert.Equal(t, &Page{offset: pageSize * 2, buffer: []byte("hello world")}, pgr.Get(2), "Get() should return the set page")
+
+	pgr.Set(20, []byte("foo bar"))
+	assert.Equal(t, &Page{offset: pageSize * 20, buffer: []byte("foo bar")}, pgr.Get(20), "Get() should return the set page")
+
+	pgr.Set(3, make([]byte, 1000))
+	assert.Equal(t, &Page{offset: pageSize * 3, buffer: make([]byte, pageSize)}, pgr.Get(3), "Set() should truncate the provided buffer down to the page size")
+}
