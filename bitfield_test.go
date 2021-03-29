5b4c2e73cdd1b3d3eaf4d94a159db15f3a85873e
Christopher Poenaru <kiambogo@gmail.com>
2021-03-29 12:22:54 -0700

    initial bit and byte functions

--- bitfield_test.go
+++ bitfield_test.go
@@ -6,13 +6,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// This is probably not testing as expected.
-func TestBitfield(t *testing.T) {
-	bitField := NewBitfield()
-	bitField.Set(0, 1)
-	bitField.Set(2, 1)
+func Test_Bitfield_SetByte(t *testing.T) {
+	t.Parallel()
 
-	assert.Equal(t, true, bitField.Get(0), "Expected bit value to be %v", 1)
-	assert.Equal(t, true, bitField.Get(2), "Expected bit value to be %v", 1)
-	assert.Equal(t, false, bitField.Get(1), "Expected bit value to be %v", 0)
+	bitField := NewBitfield(0)
+	changed := bitField.SetByte(0, 1)
+
+	assert.True(t, changed)
+	b := bitField.GetByte(0)
+	assert.Equal(t, uint8(1), b&1)
+	assert.Equal(t, uint8(0), b&8)
+
+	changed = bitField.SetByte(0, 1)
+	assert.False(t, changed)
+
+	changed = bitField.SetByte(6, 0)
+	assert.False(t, changed)
+	changed = bitField.SetByte(6, 1)
+	b = bitField.GetByte(6)
+	assert.Equal(t, uint8(1), b&1)
+	assert.Equal(t, uint8(0), b&8)
+}
+
+func Test_Bitfield_SetByteTwice(t *testing.T) {
+	t.Parallel()
+
+	bitField := NewBitfield(0)
+	changed := bitField.SetByte(0, 1)
+	assert.True(t, changed)
+	changed = bitField.SetByte(0, 1)
+	assert.False(t, changed)
+}
+
+func Test_Bitfield_SetBit(t *testing.T) {
+	bitField := NewBitfield(0)
+	changed := bitField.SetBit(0, true)
+
+	assert.True(t, changed)
+	updatedByte := bitField.GetByte(0)
+	assert.Equal(t, uint8(1), updatedByte&1)
+	assert.Equal(t, uint8(0), updatedByte&8)
 }
