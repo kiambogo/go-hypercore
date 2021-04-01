938de8be8e4a4df31e5b303987a91e877ac058c2
Christopher Poenaru <kiambogo@gmail.com>
2021-04-01 11:35:18 -0700

    wip rle

--- bitfield/rle.go
+++ bitfield/rle.go
@@ -1,7 +1,39 @@
 package bitfield
 
-func Encode() {
+import (
+	"encoding/binary"
+	"log"
+)
 
+func Encode(data []byte) ([]byte, bool) {
+	encodedData := []byte{}
+
+	if len(data) == 0 {
+		return data, false
+	}
+
+	currentRunByte := data[0]
+	var currentRunLength int64 = 1
+	for _, b := range data {
+		if b == currentRunByte {
+			currentRunLength++
+			continue
+		}
+
+		crlBuf := make([]byte, binary.MaxVarintLen64)
+		_ = binary.PutVarint(crlBuf, currentRunLength)
+		crlBuf = append(crlBuf, currentRunByte)
+		encodedData = append(encodedData, crlBuf...)
+		currentRunByte = b
+		currentRunLength = 1
+	}
+
+	if len(encodedData) >= len(data) {
+		log.Printf("encoded: %d is gt than orig: %d", len(encodedData), len(data))
+		return data, false
+	}
+
+	return encodedData, true
 }
 
 func Decode() {
